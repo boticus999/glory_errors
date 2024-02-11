b061f3fa96d953e2202f1b147bc7aac80de6211b
boticus999 <a.zherdev93@gmail.com>
2024-02-11 21:46:09 +0200

    first version

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import glob
-import pandas
-import tabula
+import telebot
+from codes import codes as c
 
-pdf_files = glob.glob('*.pdf')
-pdf_tables = tabula.read_pdf(pdf_files[1])
+bot = telebot.TeleBot('6593640929:AAE5zl8zxzL2xMooxKGuP2K29RW20q8XUpA')
 
+
+@bot.message_handler()
+def info(message):
+    bot.reply_to(message, f'{c[message.text]}')
+
+
+bot.infinity_polling()
