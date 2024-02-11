f8738adece5df905351340f848f913efa1234787
boticus999 <a.zherdev93@gmail.com>
2024-02-11 16:24:09 +0200

    dic ready

--- dic.py
+++ dic.py
@@ -1,26 +1,40 @@
+import string
 import re
 
 
 def process_word(word):
+    """
+    Функция для обработки слова: удаляет знаки препинания и приводит к нижнему регистру.
+    Если слово - четырехзначное число, то добавляет кавычки.
+    """
     if re.match(r'^\d{4}$', word):
-        word = f'"{word}": '
+        word = f'"{word}": "'
     return word
 
 
-def create_dictionary(file_path):
-    word_count = {}
+def process_line(line, end_sign='",'):
+    """
+    Функция для обработки строки: добавляет знак в конце и обрабатывает каждое слово.
+    """
+    line = line.strip() + end_sign
+    words = line.split(' ', 1)
+    processed_words = [process_word(word) for word in words]
+    processed_line = ''.join(processed_words)
+    return processed_line
+
+
+def process_file(file_path, end_sign='", '):
+    """
+    Функция для обработки всего файла и сохранения изменений обратно в файл.
+    """
     with open(file_path, 'r', encoding='utf-8') as file:
-        for line in file:
-            words = line.split()
-            for word in words:
-                word = process_word(word)
-                if word:
-                    if word in word_count:
-                        word_count[word] += 1
-                    else:
-                        word_count[word] = 1
-    return word_count
+        lines = file.readlines()
+
+    processed_lines = [process_line(line, end_sign) for line in lines]
+
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.writelines(processed_lines)
 
 
-file_path = 'your_text_file.txt'
-dictionary = create_dictionary(file_path)
+file_path = 'codes.py'
+process_file(file_path, end_sign='", ')
