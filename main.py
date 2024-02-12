0fb30111a013a14de4a5fd56552aafe16f242cfd
boticus999 <a.zherdev93@gmail.com>
2024-02-12 20:04:06 +0200

    keep alive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import telebot
+
+from bg import keep_alive
 from codes import codes as c
 
 bot = telebot.TeleBot('6593640929:AAE5zl8zxzL2xMooxKGuP2K29RW20q8XUpA')
@@ -9,4 +11,5 @@ def info(message):
     bot.reply_to(message, f'{c[message.text]}')
 
 
+keep_alive()
 bot.infinity_polling()
